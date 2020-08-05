dd14c28a739bc6f76b6676a6ec5e9231fd2209d0
Tea Cup <35646791+TheTeaCup@users.noreply.github.com>
2020-08-05 12:04:27 -0400

    Update index.js

--- src/Bot/Database/index.js
+++ src/Bot/Database/index.js
@@ -1,6 +1,5 @@
 /* Database */
 const enmap = require("enmap");
-//const enmapSQLite = require("enmap-sqlite");
 
 module.exports = Mythical => {
   console.log("[Coven] (Bot) Database Up!");
@@ -11,4 +10,12 @@ module.exports = Mythical => {
   
   Mythical.types = new enmap({ name: "types" });
   
- }
+  Mythical.applications = new enmap({ name: "staff-apps" });
+  
+  Mythical.settings = new enmap({ name: "user-settings" });
+  
+  Mythical.forum = new enmap({ name: "forums" });
+  
+  Mythical.news = new enmap({ name: "news" });
+  
+ };
